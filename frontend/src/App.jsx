58c98e731dc63dfcb935ed76813def19317255d7
Juan Urriago <j.estebanurriago@gmail.com>
2026-04-23 20:45:05 -0500

    Se elimina el caracter invisible

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { supabase } from "./lib/supabaseClient"; 
 
